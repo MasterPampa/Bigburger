09c926293857c4b4327f8a5a8ecdec3cbd6289b0
Master Pampa <styven.munhurrun@gmail.com>
2023-10-05 11:31:45 +0200

    ajout option admin

--- bigburger/src/components/header/Header.jsx
+++ bigburger/src/components/header/Header.jsx
@@ -1,7 +1,9 @@
 import './header.css'
 import Logo from '../../images/logo.png'
 
-function Header(){
+
+function Header({ isAdmin, toggleAdmin }) {
+
     return(
         <header>
             <div className='title'>
@@ -9,11 +11,16 @@ function Header(){
                 <h1>Big Burger</h1>
             </div>
             <nav>
-                <div className='admin'>
-                    <div className='admin__button'></div>
-                    <p>Activer le mode admin</p>
+                <div className={isAdmin ? 'admin connected' : 'admin'}>
+                    <div className={isAdmin ? 'admin__button switch' : 'admin__button'} onClick={toggleAdmin}></div>
+                    <p>{isAdmin ? 'Désactiver le mode admin' : 'Activer le mode admin'}</p>
                 </div>
                 <div className='profile'>
+                    <div className='profile__intro'>
+                        <h4>Hey, <span className='username'>$username</span></h4>
+                        <p>Se déconnecter</p>
+                    </div>
+                    <div className='profile__button'><i class="fa-regular fa-circle-user "></i></div>
                 </div>
             </nav>
         </header>
--- bigburger/src/pages/home/Home.jsx
+++ bigburger/src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import Header from "../../components/header/Header"
 import Cart from "../../components/cart/Cart"
 import Product from "../../components/product/Product"
+import React, {useState} from 'react';
+
 
 function Home(){
+    const [isAdmin, setIsAdmin] = useState(false);
+    const toggleAdmin = () => {
+        setIsAdmin(!isAdmin); // Inverse la valeur de isAdmin
+        console.log(isAdmin)
+    };
+
     return(
         <span>
-            <Header />
+            <Header isAdmin={isAdmin} setIsAdmin={setIsAdmin} toggleAdmin={toggleAdmin}/>
             <main>
                 <Cart />
                 <Product />
