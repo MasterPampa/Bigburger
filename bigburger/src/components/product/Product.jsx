9a6a96a7db41c3f38394f31a798991e5195f0234
Master Pampa <styven.munhurrun@gmail.com>
2023-10-10 13:31:33 +0200

    ajout cards

--- bigburger/src/components/product/Product.jsx
+++ bigburger/src/components/product/Product.jsx
@@ -3,7 +3,13 @@ function Product(){
     return(
         <div className="product">
             <div className="product__cards">
-                <article className="product__cards__card"></article>
+                <article className="product__cards__card">
+                    <div className='product__content'>
+                        <div className='product__content__image'></div>
+                        <div className='product__content__title'></div>
+                        <div className='product__content__price'></div>
+                    </div>
+                </article>
                 <article className="product__cards__card"></article>
                 <article className="product__cards__card"></article>
                 <article className="product__cards__card"></article>
